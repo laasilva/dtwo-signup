a47ccab0274134fcc231d7bd7f4dd79f4d709380
Larissa Silva <lasilva@squaretrade.com>
2020-12-21 22:30:00 -0300

    initial project structure

---
+++ signup-api/src/main/java/io/dtwo/api/signup/SignupApiApplication.java
@@ -0,0 +1,13 @@
+package io.dtwo.api.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupApiApplication.class, args);
+	}
+
+}
---
+++ signup-api/src/test/java/io/dtwo/api/signup/SignupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dtwo.api.signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
